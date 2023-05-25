7263073b8dc017405c76203c79cddff01e9e9c43
Yegor <mamitkoegorb@gmail.com>
2023-05-25 15:13:09 +0300

    3 commit

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path ('main/',name="main"),
+    path('recipe_list/',recipe_list,name="recipe_list"),
+    path('category_detail/',category_detail,name="category_detail"),
+    path('category_list/',category_list,name="category_list"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,15 +1,15 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 def main(request):
     recipes = Recipe.objects.all()
     return render(request, 'main.html', {'main': recipes})
-def category_detail(request):
-    recipes = Recipe.objects.get(name="Category")
-    return render(request, 'category_detail.html', {'category_detail': recipes})
+def category_detail(request, title):
+    recipes = get_object_or_404(Recipe, title=title)
+    return render(request, 'category_detail.html', {'recipes': recipes})
 def category_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'category_list.html', {'category_list': recipes})
+    return render(request, 'category_list.html', {'recipes': recipes})
 def recipe_list(request):
-    recipes = Recipe.objects.get()
-    return render(request, 'recipe_detail.html', {'recipe_detail': recipes})
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe_detail.html', {'recipe': recipes})
