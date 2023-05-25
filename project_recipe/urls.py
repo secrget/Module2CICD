ad7dd41763a997e709f3c1ad99067ce99d6dd4c8
Yegor <mamitkoegorb@gmail.com>
2023-05-25 15:01:17 +0300

    view commit

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,15 @@
-
+import random
+from django.shortcuts import render
+from .models import Recipe
+def main(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'main.html', {'main': recipes})
+def category_detail(request):
+    recipes = Recipe.objects.get(name="Category")
+    return render(request, 'category_detail.html', {'category_detail': recipes})
+def category_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'category_list.html', {'category_list': recipes})
+def recipe_list(request):
+    recipes = Recipe.objects.get()
+    return render(request, 'recipe_detail.html', {'recipe_detail': recipes})
